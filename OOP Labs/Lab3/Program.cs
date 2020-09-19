d016e6ce47fec70c965885848e652afd845b2c69
eldarian-1 <ciborg.brain@gmail.com>
2020-09-20 00:07:49 +0500

    0:07 20.09.2020

---
+++ OOP Labs/Lab3/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Lab3
+{
+    class Program
+    {
+        private const int c_iN = 30;
+        private const double c_dMin = 0.1d;
+        private const double c_dMax = 1d;
+        private const double c_dK = 10d;
+        private const double c_dD = (c_dMax - c_dMin) / c_dK;
+        private const double c_dE = 0.0001;
+        private const string c_sOutput = "x = {0}; Sn = {1}; Se = {2}; y = f(x) = {3}";
+
+        public static void Main(string[] args)
+        {
+            Run();
+            Console.ReadKey();
+        }
+
+        private static void Run()
+        {
+            for(double x = c_dMin; x <= c_dMax; x += c_dD)
+            {
+                Console.WriteLine(
+                    c_sOutput, x,
+                    SeriesArithmetic(x),
+                    SeriesDifferential(x),
+                    Function(x));
+            }
+        }
+
+        private static double Function(double x)
+        {
+            return (1 + Math.Pow(x, 2)) / 2 * Math.Atan(x) - x / 2;
+        }
+
+        private static double Series(double x, int n)
+        {
+            return Math.Pow(-1, n + 1) * Math.Pow(x, 2 * n + 1) / (4 * Math.Pow(n, 2) - 1);
+        }
+
+        private static double SeriesArithmetic(double x)
+        {
+            double Sum = 0;
+            for (int i = 1; i <= c_iN; ++i)
+                Sum += Series(x, i);
+            return Sum;
+        }
+
+        private static double SeriesDifferential(double x)
+        {
+            int i = 1;
+            double Sum = 0;
+            double Result = Function(x);
+            while(Math.Abs(Sum - Result) > c_dE)
+                Sum += Series(x, i++);
+            return Sum;
+        }
+    }
+}
---
+++ OOP Labs/Lab4/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace Lab4
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Formation(out int[] array, 100);
+            Output(array);
+            DeleteElems(ref array);
+            Output(array);
+            Addition(ref array, 5, 7);
+            Output(array);
+            Reverse(array);
+            Output(array);
+            FindElem(array, 29);
+            InsertSort(array);
+            Output(array);
+            Console.ReadKey();
+        }
+
+        private static void Output(int[] array)
+        {
+            for (int i = 0, k = array.Length; i < k; ++i)
+                Console.Write("{0} ",array[i]);
+            Console.WriteLine("\n");
+        }
+
+        private static void Formation(out int[] array, int n)
+        {
+            Random rand = new Random();
+            array = new int[n];
+            for (int i = 0; i < n; ++i)
+                array[i] = rand.Next();
+        }
+
+        private static void DeleteElems(ref int[] array)
+        {
+            int[] temp = array;
+            int n = temp.Length / 2;
+            array = new int[n];
+            for (int i = 0; i < n; ++i)
+                array[i] = temp[i * 2];
+        }
+
+        private static void Addition(ref int[] array, int n, int k)
+        {
+            Random rand = new Random();
+            int[] temp = array;
+            int sizeT = temp.Length;
+            int sizeN = sizeT + n;
+            array = new int[sizeN];
+            for (int i = 0; i < k; ++i)
+                array[i] = temp[i];
+            for (int i = k, j = k + n; i < j; ++i)
+                array[i] = rand.Next();
+            for (int i = k + n; i < sizeN; ++i)
+                array[i] = temp[i - n];
+        }
+
+        private static void Reverse(int[] array)
+        {
+            int temp;
+            int N = array.Length - 1;
+            int n = N / 2;
+            for (int i = 0; i < n; ++i)
+            {
+                temp = array[i];
+                array[i] = array[N - i];
+                array[N - i] = temp;
+            }
+        }
+
+        private static void FindElem(int[] array, int k)
+        {
+            try
+            {
+                int n = 0;
+                while (array[n++] != k) ;
+                Console.WriteLine("Элемент найден по индексу {0}", n);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Элемент не найден");
+            }
+            finally
+            {
+                Console.WriteLine();
+            }
+        }
+
+        private static void InsertSort(int[] array)
+        {
+            int i, j, temp, k = array.Length;
+            for (i = 0; i < k; i++)
+            {
+                temp = array[i];
+                for (j = i - 1; j >= 0 && array[j] > temp; j--)
+                    array[j + 1] = array[j];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}

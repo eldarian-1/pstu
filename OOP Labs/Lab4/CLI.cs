eed61b1df69a1e48f6628564d02d1661d697916c
eldarian-1 <ciborg.brain@gmail.com>
2020-10-04 13:07:21 +0500

    13:07 04.10.2020

---
+++ OOP Labs/Lab4/CLI.cs
@@ -0,0 +1,170 @@
+﻿using System;
+
+namespace Lab4
+{
+    delegate void Task();
+
+    class CLI
+    {
+        public const char c_cN = 'N';
+        public const char c_cK = 'K';
+        public const char c_cA = 'A';
+
+        private const int c_iMinK = 1;
+        private const int c_iMaxK = 5;
+        private const int c_iMinArray = 10;
+        private const int c_iMaxArray = 20;
+        private const int c_iMinNumber = 100;
+        private const int c_iMaxNumber = 999;
+        public const int c_iMaxInt = 2147483647;
+
+        private const string c_sElem = "{0} ";
+        private const string c_sReadNumber = "Введите {0}: ";
+        private const string c_sReadNumberA = "Введите {0}{1}: ";
+        private const string c_sGetTask =
+            "МЕНЮ\n" +
+            "1. Создать массив\n" +
+            "2. Удалить четные элементы в массиве\n" +
+            "3. Добавить n элементов с k элемента\n" +
+            "4. Перевернуть массив\n" +
+            "5. Поиск элемента\n" +
+            "6. Сортировка массива\n" +
+            "0. Выход\n" +
+            "Выберете действие: ";
+        private const string c_sGetMode = "Введите способ получения чисел (1 - ввод, - 2 случайное): ";
+        public const string c_sFoundElem = "Порядковый номер искомого элемента: {0}";
+        public const string c_sNotFound = "Элемент не найден";
+        public const string c_sNullArray = "Ошибка! Массив не создан.\n";
+
+        private static Random s_rand = new Random();
+
+        public static void Run()
+        {
+            Task task = null;
+            do
+            {
+                task = GetTask();
+                try
+                {
+                    task();
+                }
+                catch(NullArrayException)
+                {
+                    Console.WriteLine(c_sNullArray);
+                }
+            }
+            while (task != null);
+        }
+
+        // Запись числа со стандартного потока ввода в выходной параметр
+        public static void ReadNum(out int number, char simbol, int i)
+        {
+            number = 0;
+            for (bool flag = false; !flag;)
+            {
+                if (simbol == c_cN || simbol == c_cK)
+                    Console.Write(c_sReadNumber, simbol);
+                else
+                    Console.Write(c_sReadNumberA, simbol, i);
+                string sNum = Console.ReadLine();
+                flag = int.TryParse(sNum, out number);
+            }
+        }
+
+        // Запись случайного числа в выходной параметр
+        public static void RandNum(out int number, char simbol, int i)
+        {
+            if (simbol == c_cN)
+                number = s_rand.Next(c_iMinArray, c_iMaxArray);
+            else if (simbol == c_cK)
+                number = s_rand.Next(c_iMinK, c_iMaxK);
+            else
+                number = s_rand.Next(c_iMinNumber, c_iMaxNumber);
+        }
+
+        private static GetNumber GetMode()
+        {
+            string key = "";
+            do
+            {
+                Console.Write(c_sGetMode);
+                key = Console.ReadLine();
+            } while (key != "1" && key != "2");
+            if (key == "1")
+                return ReadNum;
+            else
+                return RandNum;
+        }
+
+        // Вывод массива
+        private static void Output(int[] array)
+        {
+            for (int i = 0, k = array.Length; i < k; ++i)
+                Console.Write(c_sElem, array[i]);
+            Console.WriteLine("\n");
+        }
+
+        private static void Formation()
+        {
+            Kernel.Formation(GetMode());
+            Output(Kernel.array);
+        }
+
+        private static void DeleteElems()
+        {
+            Kernel.DeleteElems();
+            Output(Kernel.array);
+        }
+
+        private static void Addition()
+        {
+            Kernel.Addition(GetMode);
+            Output(Kernel.array);
+        }
+
+        private static void Reverse()
+        {
+            Kernel.Reverse();
+            Output(Kernel.array);
+        }
+
+        private static void FindElem()
+        {
+            Kernel.FindElem(GetMode);
+        }
+
+        private static void InsertSort()
+        {
+            Kernel.InsertSort();
+            Output(Kernel.array);
+        }
+
+        private static Task GetTask()
+        {
+            bool flag = true;
+            while(flag)
+            {
+                Console.Write(c_sGetTask);
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        return Formation;
+                    case "2":
+                        return DeleteElems;
+                    case "3":
+                        return Addition;
+                    case "4":
+                        return Reverse;
+                    case "5":
+                        return FindElem;
+                    case "6":
+                        return InsertSort;
+                    case "0":
+                        flag = false;
+                        break;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ OOP Labs/Lab4/Kernel.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+namespace Lab4
+{
+    class NullArrayException : Exception { }
+
+    delegate void GetNumber(out int number, char simbol, int i = -1);
+    delegate GetNumber ModeGetNumber();
+    delegate bool IsValidate(int x, int top);
+
+    class Kernel
+    {
+        public static int[] array = null;
+        private static IsValidate IsValid = (x, top) => (x >= 0 && x < top);
+
+        // Получение валидного числа
+        private static void GetValid(out int number, GetNumber GetNum, char simbol, int top)
+        {
+            do GetNum(out number, simbol);
+            while (!IsValid(number, top));
+        }
+
+        private static void CheckArray()
+        {
+            if (array == null)
+                throw new NullArrayException();
+        }
+
+        // Создание массива
+        public static void Formation(GetNumber GetNum)
+        {
+            GetValid(out int n, GetNum, CLI.c_cN, CLI.c_iMaxInt);
+            array = new int[n];
+            for (int i = 0; i < n; ++i)
+                GetNum(out array[i], CLI.c_cA, i);
+        }
+
+        // Удаление всех элементов с четными индексами
+        public static void DeleteElems()
+        {
+            CheckArray();
+            int[] temp = array;
+            int n = temp.Length / 2;
+            array = new int[n];
+            for (int i = 0; i < n; ++i)
+                array[i] = temp[i * 2 + 1];
+        }
+
+        // Добавление n элементов, начиная с k
+        public static void Addition(ModeGetNumber ModeGetNum)
+        {
+            CheckArray();
+            GetNumber GetNum = ModeGetNum();
+            int oldSize = array.Length;
+            GetValid(out int n, GetNum, CLI.c_cN, CLI.c_iMaxInt - oldSize);
+            GetValid(out int k, GetNum, CLI.c_cK, oldSize);
+            int newSize = oldSize + n;
+            int[] temp = array;
+            array = new int[newSize];
+            for (int i = 0; i < k; ++i)
+                array[i] = temp[i];
+            for (int i = k, j = k + n; i < j; ++i)
+                GetNum(out array[i], CLI.c_cA, i);
+            for (int i = k + n; i < newSize; ++i)
+                array[i] = temp[i - n];
+        }
+
+        // Переворот массива
+        public static void Reverse()
+        {
+            CheckArray();
+            int N = array.Length - 1;
+            int n = N / 2;
+            for (int i = 0; i < n; ++i)
+            {
+                int temp = array[i];
+                array[i] = array[N - i];
+                array[N - i] = temp;
+            }
+        }
+
+        // Поиск элемента по значению
+        public static void FindElem(ModeGetNumber ModeGetNum)
+        {
+            CheckArray();
+            GetNumber GetNum = ModeGetNum();
+            GetNum(out int k, CLI.c_cK);
+            int n = 0;
+            try
+            {
+                while (array[n++] != k) ;
+                Console.WriteLine(CLI.c_sFoundElem, n);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine(CLI.c_sNotFound);
+            }
+            finally
+            {
+                Console.WriteLine();
+            }
+        }
+
+        // Сортировка вставками
+        public static void InsertSort()
+        {
+            CheckArray();
+            int i, j, temp, n = array.Length;
+            for (i = 0; i < n; i++)
+            {
+                temp = array[i];
+                for (j = i - 1; j >= 0 && array[j] > temp; j--)
+                    array[j + 1] = array[j];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}

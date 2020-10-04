754e357aafc39718e0fcb126574161b86361c28c
eldarian-1 <ciborg.brain@gmail.com>
2020-10-04 15:49:26 +0500

    15:49 04.10.2020

---
+++ OOP Labs/Lab5/Lab5/CLI.cs
@@ -0,0 +1,215 @@
+﻿using System;
+
+namespace Lab5
+{
+    delegate void Task();
+
+    class CLI
+    {
+        public const char c_cN = 'N';
+        public const char c_cM = 'M';
+        public const char c_cK = 'K';
+        public const char c_cA = 'A';
+
+        private const int c_iMinK = 1;
+        private const int c_iMaxK = 5;
+        private const int c_iMinArray = 10;
+        private const int c_iMaxArray = 20;
+        private const int c_iMinNumber = 100;
+        private const int c_iMaxNumber = 999;
+
+        private const string c_sElem = "{0} ";
+        private const string c_sReadNumber = "Введите {0}: ";
+        private const string c_sReadNumberA = "Введите {0}{1}: ";
+        private const string c_sReadNumberA2 = "Введите {0}({1},{2}): ";
+        private const string c_sGetTask =
+            "МЕНЮ\n" +
+            "Одномерный массив\n" +
+            "\t1. вывести массив\n" +
+            "\t2. cоздать массив\n" +
+            "\t3. добавить N элементов, начиная с K элемента\n" +
+            "Двемерный массив\n" +
+            "\t4. вывести массив\n" +
+            "\t5. создать массив\n" +
+            "\t6. удалить чентные строки в массиве\n" +
+            "Рваный массив\n" +
+            "\t7. вывести массив\n" +
+            "\t8. создать массив\n" +
+            "\t9. добавть строку в конец массива\n" +
+            "0. Выход\n" +
+            "Выберете действие: ";
+        private const string c_sGetMode = "Введите способ получения чисел (1 - ввод, - 2 случайное): ";
+        public const string c_sNullArray = "Ошибка! Массив не создан.\n";
+
+        private static Random s_rand = new Random();
+
+        public static void Run()
+        {
+            Task task = null;
+            do
+            {
+                task = GetTask();
+                try
+                {
+                    task();
+                }
+                catch (NullArrayException)
+                {
+                    Console.WriteLine(c_sNullArray);
+                }
+            }
+            while (task != null);
+        }
+
+        public static void ReadNum(out int number, char simbol, int i, int j)
+        {
+            number = 0;
+            for (bool flag = false; !flag;)
+            {
+                if (i == -1)
+                    Console.Write(c_sReadNumber, simbol);
+                else if (j == -1)
+                    Console.Write(c_sReadNumberA, simbol, i);
+                else
+                    Console.Write(c_sReadNumberA2, simbol, i, j);
+                string sNum = Console.ReadLine();
+                flag = int.TryParse(sNum, out number);
+            }
+        }
+
+        public static void RandNum(out int number, char simbol, int i, int j)
+        {
+            if (simbol == c_cN || simbol == c_cM)
+                number = s_rand.Next(c_iMinArray, c_iMaxArray);
+            else if (simbol == c_cK)
+                number = s_rand.Next(c_iMinK, c_iMaxK);
+            else
+                number = s_rand.Next(c_iMinNumber, c_iMaxNumber);
+        }
+
+        private static GetNumber GetMode()
+        {
+            string key = "";
+            do
+            {
+                Console.Write(c_sGetMode);
+                key = Console.ReadLine();
+            } while (key != "1" && key != "2");
+            if (key == "1")
+                return ReadNum;
+            else
+                return RandNum;
+        }
+
+        public static void Output(int[] array)
+        {
+            Kernel.CheckArray(Kernel.array1);
+            for (int i = 0, n = array.Length; i < n; ++i)
+                Console.Write(c_sElem, array[i]);
+            Console.WriteLine();
+        }
+
+        public static void Output(int[,] array)
+        {
+            Kernel.CheckArray(Kernel.array2);
+            int n = array.GetUpperBound(0) + 1;
+            int k = array.Length / n;
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < k; ++j)
+                    Console.Write(c_sElem, array[i, j]);
+                Console.WriteLine();
+            }
+        }
+
+        public static void Output(int[][] array)
+        {
+            Kernel.CheckArray(Kernel.array3);
+            for (int i = 0, n = array.Length; i < n; ++i)
+            {
+                for (int j = 0, k = array[i].Length; j < k; ++j)
+                    Console.Write(c_sElem, array[i][j]);
+                Console.WriteLine();
+            }
+        }
+
+        public static void Output1()
+        {
+            Output(Kernel.array1);
+        }
+
+        public static void Output2()
+        {
+            Output(Kernel.array2);
+        }
+
+        public static void Output3()
+        {
+            Output(Kernel.array3);
+        }
+
+        public static void Formation1()
+        {
+            Kernel.Formation1(GetMode());
+        }
+
+        public static void Formation2()
+        {
+            Kernel.Formation2(GetMode());
+        }
+
+        public static void Formation3()
+        {
+            Kernel.Formation3(GetMode());
+        }
+
+        public static void Operation1()
+        {
+            Kernel.Addition1(GetMode());
+        }
+
+        public static void Operation2()
+        {
+            Kernel.DeleteElems2();
+        }
+
+        public static void Operation3()
+        {
+            Kernel.Addition3(GetMode());
+        }
+
+        private static Task GetTask()
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.Write(c_sGetTask);
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        return Output1;
+                    case "2":
+                        return Formation1;
+                    case "3":
+                        return Operation1;
+                    case "4":
+                        return Output2;
+                    case "5":
+                        return Formation2;
+                    case "6":
+                        return Operation2;
+                    case "7":
+                        return Output3;
+                    case "8":
+                        return Formation3;
+                    case "9":
+                        return Operation3;
+                    case "0":
+                        flag = false;
+                        break;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ OOP Labs/Lab5/Lab5/Kernel.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace Lab5
+{
+    delegate void GetNumber(out int number, char simbol, int i = -1, int j = -1);
+
+    class NullArrayException : Exception { }
+
+    class Kernel
+    {
+        public static int[] array1 { get; set; }
+        public static int[,] array2 { get; set; }
+        public static int[][] array3 { get; set; }
+
+        public static void CheckArray<T>(T array)
+        {
+            if (array == null)
+                throw new NullArrayException();
+        }
+
+        public static void Formation1(GetNumber GetNum)
+        {
+            GetNum(out int n, CLI.c_cN);
+            array1 = new int[n];
+            for (int i = 0; i < n; ++i)
+                GetNum(out array1[i], CLI.c_cA);
+        }
+
+        public static void Formation2(GetNumber GetNum)
+        {
+            GetNum(out int n, CLI.c_cN);
+            GetNum(out int m, CLI.c_cM);
+            array2 = new int[n, m];
+            for (int i = 0; i < n; ++i)
+                for (int j = 0; j < m; ++j)
+                    GetNum(out array2[i, j], CLI.c_cA, i, j);
+        }
+
+        public static void Formation3( GetNumber GetNum)
+        {
+            GetNum(out int n, CLI.c_cN);
+            array3 = new int[n][];
+            for (int i = 0; i < n; ++i)
+            {
+                GetNum(out int m, CLI.c_cM);
+                array3[i] = new int[m];
+                for (int j = 0; j < m; ++j)
+                    GetNum(out array3[i][j], CLI.c_cA, i, j);
+            }
+        }
+
+        public static void Addition1(GetNumber GetNum)
+        {
+            CheckArray(array1);
+            GetNum(out int n, CLI.c_cN);
+            GetNum(out int k, CLI.c_cK);
+            int[] temp = array1;
+            int sizeT = temp.Length;
+            int sizeN = sizeT + n;
+            array1 = new int[sizeN];
+            for (int i = 0; i < k; ++i)
+                array1[i] = temp[i];
+            for (int i = k, j = k + n; i < j; ++i)
+                GetNum(out array1[i], CLI.c_cA, i);
+            for (int i = k + n; i < sizeN; ++i)
+                array1[i] = temp[i - n];
+        }
+
+        public static void DeleteElems2()
+        {
+            CheckArray(array2);
+            int[,] temp = array2;
+            int n = array2.GetUpperBound(0) + 1;
+            int k = array2.Length / n;
+            int m = n / 2;
+            array2 = new int[m, k];
+            for (int i = 0; i < m; ++i)
+                for (int j = 0; j < k; ++j)
+                    array2[i, j] = temp[i * 2, j];
+        }
+
+        public static void Addition3(GetNumber GetNum)
+        {
+            CheckArray(array3);
+            int[][] temp = array3;
+            int n = temp.Length;
+            array3 = new int[n + 1][];
+            for (int i = 0; i < n; ++i)
+                array3[i] = temp[i];
+            GetNum(out int m, CLI.c_cM);
+            array3[n] = new int[m];
+            for (int i = 0; i < m; ++i)
+                GetNum(out array3[n][i], CLI.c_cA, n, i);
+        }
+    }
+}

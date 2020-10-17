d3bbd5b2b519433048a6d35dcf16dc629842bb4a
eldarian-1 <ciborg.brain@gmail.com>
2020-10-17 20:43:47 +0500

    20:43 17.10.2020

--- OOP Labs/Lab7/Lab7/MainWindow.xaml.cs
+++ OOP Labs/Lab7/Lab7/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -18,56 +19,130 @@ namespace Lab7
 {
     public partial class MainWindow : Window
     {
-        public ObservableCollection<Array1D> Array1Ds { get;set; }
-        public ObservableCollection<ObservableCollection<Array1D>> Array2Ds { get;set; }
+        private int[] Array1D;
+        private int[,] Array2D;
+        private int[][] ArrayJagged;
 
         public MainWindow()
         {
             InitializeComponent();
-            Array1Ds = new ObservableCollection<Array1D>()
-            {
-                new Array1D{Text = "Хуита"},
-                new Array1D{Text = "Хуита"},
-                new Array1D{Text = "Хуита"}
+            Array1D = new int[] { 13, 14, 15 };
+            Array2D = new int[3, 4] { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
+            ArrayJagged = new int[][] {
+                new int[] { 1, 2, 3, 4 },
+                new int[] { 1, 2, 3 },
+                new int[] { 1, 2, 3, 4, 5 }
             };
-            Array2Ds = new ObservableCollection<ObservableCollection<Array1D>>()
-            {
-                new ObservableCollection<Array1D>()
-            {
-                new Array1D{Text = "Хуита"},
-                new Array1D{Text = "Хуита"},
-                new Array1D{Text = "Хуита"}
-            },
-                new ObservableCollection<Array1D>()
-            {
-                new Array1D{Text = "Хуита"},
-                new Array1D{Text = "Хуита"},
-                new Array1D{Text = "Хуита"}
-            },
-                new ObservableCollection<Array1D>()
-            {
-                new Array1D{Text = "Хуита"},
-                new Array1D{Text = "Хуита"},
-                new Array1D{Text = "Хуита"}
-            }
-            };
-            DataContext = this;
-
-            JaggedArray.Children.Add(new TextBox { Text = "Custom" });
+            SetArray1D();
+            SetArray2D();
+            SetArrayJagged();
         }
 
-        private void ChangeArray1Size(object sender, RoutedEventArgs e)
+        private void ChangeArray1DSize(object sender, RoutedEventArgs e)
         {
-            if(int.TryParse(Array1Size.Text, out int size))
+            if (int.TryParse(Array1Size.Text, out int size))
             {
-                int count = Array1Ds.Count;
-                if (count > size)
-                    for (int i = 0, n = count - size; i < n; ++i)
-                        Array1Ds.RemoveAt(Array1Ds.Count - 1);
-                else if (count < size)
-                    for (int i = 0, n = size - count; i < n; ++i)
-                        Array1Ds.Add(new Array1D { Text = "Хуита" });
+                Array1D = new int[size];
+                for (int i = 0; i < size; i++)
+                    Array1D[i] = i;
+                SetArray1D();
             }
+        }
+
+        private void ChangeArray2DSize(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(Array2SizeN.Text, out int n) &&
+                int.TryParse(Array2SizeM.Text, out int m))
+            {
+                Array2D = new int[n, m];
+                for (int i = 0; i < n; i++)
+                    for (int j = 0; j < m; j++)
+                        Array2D[i, j] = i * m + j;
+                SetArray2D();
+            }
+        }
+
+        private void ChangeArrayJaggedSize(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(Array2SizeN.Text, out int n) &&
+                int.TryParse(Array2SizeM.Text, out int m))
+            {
+                Array2D = new int[n, m];
+                for (int i = 0; i < n; i++)
+                    for (int j = 0; j < m; j++)
+                        Array2D[i, j] = i * m + j;
+                SetArray2D();
+            }
+        }
+
+        private void SetArray1D()
+        {
+            ListBoxArray1D.Items.Clear();
+            for (int i = 0, n = Array1D.Length; i < n; ++i)
+                ListBoxArray1D.Items.Add(new ListBoxItem { Content = new TextBox { Text = Array1D[i].ToString() } });
+        }
+
+        private void SetArray2D()
+        {
+            TreeViewArray2D.Items.Clear();
+            int n = Array2D.GetUpperBound(0) + 1;
+            int m = Array2D.Length / n;
+            for (int i = 0; i < n; ++i)
+            {
+                WrapPanel tempWrapPanel = new WrapPanel();
+                TreeViewItem tempItem = new TreeViewItem { Header = "Строка " + i};
+                TreeViewArray2D.Items.Add(tempItem);
+                TreeViewArray2D.Items.Add(tempWrapPanel);
+                for (int j = 0; j < m; ++j)
+                {
+                    string tempText = Array2D[i, j].ToString();
+                    TextBox tempTextBox = new TextBox { Text = tempText };
+                    tempWrapPanel.Children.Add(tempTextBox);
+                }
+            }
+        }
+
+        private void SetArrayJagged()
+        {
+            TreeViewArrayJagged.Items.Clear();
+            int n = ArrayJagged.Length;
+            for (int i = 0; i < n; ++i)
+            {
+                int m = ArrayJagged[i].Length;
+                WrapPanel tempWrapPanel = new WrapPanel();
+                Label tempLabel = new Label { Content = $"Строка {i} M:" };
+                TextBox tempTextBox = new TextBox { Text = m.ToString() };
+                Button tempButton = new Button { Content = $"Изменить{i}" };
+                StackPanel tempStackPanel = new StackPanel { Orientation = Orientation.Horizontal };
+                TreeViewItem tempItem = new TreeViewItem { Header = tempStackPanel };
+                tempButton.Click += ChangeArrayJaggedSize;
+                tempStackPanel.Children.Add(tempLabel);
+                tempStackPanel.Children.Add(tempTextBox);
+                tempStackPanel.Children.Add(tempButton);
+                TreeViewArrayJagged.Items.Add(tempItem);
+                TreeViewArrayJagged.Items.Add(tempWrapPanel);
+                for (int j = 0; j < m; ++j)
+                {
+                    string tempText = ArrayJagged[i][j].ToString();
+                    TextBox tempEdit = new TextBox { Text = tempText };
+                    tempWrapPanel.Children.Add(tempEdit);
+                }
+            }
+        }
+
+        private ListBox GetListBox()
+        {
+            string xaml = @"<ListBox
+                                xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'
+                                xmlns:x='http://schemas.microsoft.com/winfx/2006/xaml'
+                                Grid.Column='1' ScrollViewer.HorizontalScrollBarVisibility='Disabled'>
+                                <ListBox.ItemsPanel>
+                                    <ItemsPanelTemplate>
+                                        <WrapPanel />
+                                    </ItemsPanelTemplate>
+                                </ListBox.ItemsPanel>
+                            </ListBox >";
+            return XamlReader.Parse(xaml) as ListBox;
         }
     }
 }

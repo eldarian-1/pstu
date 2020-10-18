a87d8ff61d9ac858b025f8207c81d34cf180830d
eldarian-1 <ciborg.brain@gmail.com>
2020-10-18 13:19:42 +0500

    13:19 18.10.2020

---
+++ OOP Labs/Lab7/Lab7/Kernel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    static class Kernel
+    {
+        public static int[] Array1D
+            = new int[] { 13, 14, 15 };
+
+        public static int[,] Array2D = new int[3, 4] {
+            { 1, 2, 3, 4 },
+            { 5, 6, 7, 8 },
+            { 9, 10, 11, 12 }
+        };
+
+        public static int[][] ArrayJagged = new int[][] {
+            new int[] { 1, 2, 3, 4 },
+            new int[] { 1, 2, 3 },
+            new int[] { 1, 2, 3, 4, 5 }
+        };
+
+        // Добавление N элементов после K элемента в одномерный массив
+        public static void Addition(ref int[] array, int n, int k)
+        {
+            int newSize = array.Length + n;
+            int[] temp = array;
+            array = new int[newSize];
+            for (int i = 0; i < k; ++i)
+                array[i] = temp[i];
+            for (int i = k, j = k + n; i < j; ++i)
+                array[i] = 0;
+            for (int i = k + n; i < newSize; ++i)
+                array[i] = temp[i - n];
+        }
+
+        // Удаление четных строк в двумерном массиве
+        public static void DropEven(ref int[,] array)
+        {
+            int[,] temp = array;
+            int n = array.GetUpperBound(0) + 1;
+            int k = array.Length / n;
+            int m = n / 2;
+            array = new int[m, k];
+            for (int i = 0; i < m; ++i)
+                for (int j = 0; j < k; ++j)
+                    array[i, j] = temp[i * 2 + 1, j];
+        }
+
+        // Добавление строки в конец рваного массива
+        public static void AdditionLine(ref int[][] array)
+        {
+            int[][] temp = array;
+            int n = temp.Length;
+            array = new int[n + 1][];
+            for (int i = 0; i < n; ++i)
+                array[i] = temp[i];
+            array[n] = new int[] { 0 };
+        }
+    }
+}
--- OOP Labs/Lab7/Lab7/MainWindow.xaml.cs
+++ OOP Labs/Lab7/Lab7/MainWindow.xaml.cs
@@ -26,13 +26,9 @@ namespace Lab7
         public MainWindow()
         {
             InitializeComponent();
-            Array1D = new int[] { 13, 14, 15 };
-            Array2D = new int[3, 4] { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
-            ArrayJagged = new int[][] {
-                new int[] { 1, 2, 3, 4 },
-                new int[] { 1, 2, 3 },
-                new int[] { 1, 2, 3, 4, 5 }
-            };
+            Array1D = Kernel.Array1D;
+            Array2D = Kernel.Array2D;
+            ArrayJagged = Kernel.ArrayJagged;
             SetArray1D();
             SetArray2D();
             SetArrayJagged();
@@ -40,13 +36,15 @@ namespace Lab7
 
         private void ChangeArray1DSize(object sender, RoutedEventArgs e)
         {
-            if (int.TryParse(Array1Size.Text, out int size))
+            if (int.TryParse(Array1DSize.Text, out int size))
             {
                 Array1D = new int[size];
                 for (int i = 0; i < size; i++)
                     Array1D[i] = i;
                 SetArray1D();
             }
+            else
+                MessageBox.Show($"Некорректное значение поля!");
         }
 
         private void ChangeArray2DSize(object sender, RoutedEventArgs e)
@@ -60,6 +58,8 @@ namespace Lab7
                         Array2D[i, j] = i * m + j;
                 SetArray2D();
             }
+            else
+                MessageBox.Show($"Некорректное значение поля!");
         }
 
         private void ChangeArrayJaggedSize(object sender, RoutedEventArgs e)
@@ -74,6 +74,11 @@ namespace Lab7
             }
             else if (int.TryParse(ArrayJaggedSize.Text, out int size))
                 ChangeArrayJaggedToSize(size);
+            else
+            {
+                MessageBox.Show($"Некорректное значение поля!");
+                return;
+            }
             SetArrayJagged();
         }
 
@@ -107,11 +112,99 @@ namespace Lab7
             }
         }
 
+        private void FixArray1D()
+        {
+            for (int i = 0, n = Array1D.Length; i < n; ++i)
+            {
+                TextBox tempTextBox = FindChild<TextBox>(Application.Current.MainWindow, "A1D" + i);
+                if (int.TryParse(tempTextBox.Text, out int num))
+                    Array1D[i] = num;
+                else
+                {
+                    MessageBox.Show($"Некорректное поле под индексом [{i}]!");
+                    break;
+                }
+            }
+        }
+
+        private void FixArray2D()
+        {
+            int n = Array2D.GetUpperBound(0) + 1;
+            int m = Array2D.Length / n;
+            for (int i = 0; i < n; ++i)
+                for (int j = 0; j < m; ++j)
+                {
+                    TextBox tempTextBox = FindChild<TextBox>(Application.Current.MainWindow, "A2D" + i + "_" + j);
+                    if (int.TryParse(tempTextBox.Text, out int num))
+                        Array2D[i, j] = num;
+                    else
+                    {
+                        MessageBox.Show($"Некорректное поле под индексом [{i},{j}]!");
+                        break;
+                    }
+                }
+        }
+
+        private void FixArrayJagged()
+        {
+            for (int i = 0, n = ArrayJagged.Length; i < n; ++i)
+                for (int j = 0, m = ArrayJagged[i].Length; j < m; ++j)
+                {
+                    TextBox tempTextBox = FindChild<TextBox>(Application.Current.MainWindow, "AJ" + i + "_" + j);
+                    if (int.TryParse(tempTextBox.Text, out int num))
+                        ArrayJagged[i][j] = num;
+                    else
+                    {
+                        MessageBox.Show($"Некорректное поле под индексом [{i}][{j}]!");
+                        break;
+                    }
+                }
+        }
+
+        private void AddLineArray1D(object sender, RoutedEventArgs e)
+        {
+            FixArray1D();
+            if (int.TryParse(Array1DN.Text, out int n) && int.TryParse(Array1DK.Text, out int k))
+                Kernel.Addition(ref Array1D, n, k);
+            else
+            {
+                MessageBox.Show($"Некорректное значение поля!");
+                return;
+            }
+            SetArray1D();
+        }
+
+        private void DropEvenArray2D(object sender, RoutedEventArgs e)
+        {
+            FixArray2D();
+            Kernel.DropEven(ref Array2D);
+            SetArray2D();
+        }
+
+        private void AddLineArrayJagged(object sender, RoutedEventArgs e)
+        {
+            FixArrayJagged();
+            Kernel.AdditionLine(ref ArrayJagged);
+            SetArrayJagged();
+        }
+
+        private void SaveToFile(object sender, RoutedEventArgs e)
+        {
+            FixArray1D();
+            FixArray2D();
+            FixArrayJagged();
+        }
+
+        private void LoadFromFile(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
         private void SetArray1D()
         {
             ListBoxArray1D.Items.Clear();
             for (int i = 0, n = Array1D.Length; i < n; ++i)
-                ListBoxArray1D.Items.Add(new ListBoxItem { Content = new TextBox { Text = Array1D[i].ToString() } });
+                ListBoxArray1D.Items.Add(new ListBoxItem { Content = new TextBox { Name = "A1D" + i, Text = Array1D[i].ToString() } });
         }
 
         private void SetArray2D()
@@ -128,7 +221,7 @@ namespace Lab7
                 for (int j = 0; j < m; ++j)
                 {
                     string tempText = Array2D[i, j].ToString();
-                    TextBox tempTextBox = new TextBox { Text = tempText };
+                    TextBox tempTextBox = new TextBox { Name = "A2D" + i + "_" + j, Text = tempText };
                     tempWrapPanel.Children.Add(tempTextBox);
                 }
             }
@@ -156,25 +249,10 @@ namespace Lab7
                 for (int j = 0; j < m; ++j)
                 {
                     string tempText = ArrayJagged[i][j].ToString();
-                    TextBox tempEdit = new TextBox { Text = tempText };
+                    TextBox tempEdit = new TextBox { Name = "AJ" + i + "_" + j, Text = tempText };
                     tempWrapPanel.Children.Add(tempEdit);
                 }
             }
-        }
-
-        private ListBox GetListBox()
-        {
-            string xaml = @"<ListBox
-                                xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'
-                                xmlns:x='http://schemas.microsoft.com/winfx/2006/xaml'
-                                Grid.Column='1' ScrollViewer.HorizontalScrollBarVisibility='Disabled'>
-                                <ListBox.ItemsPanel>
-                                    <ItemsPanelTemplate>
-                                        <WrapPanel />
-                                    </ItemsPanelTemplate>
-                                </ListBox.ItemsPanel>
-                            </ListBox >";
-            return XamlReader.Parse(xaml) as ListBox;
         }
 
         public static T FindChild<T>(DependencyObject parent, string childName)

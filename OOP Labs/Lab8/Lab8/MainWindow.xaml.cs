f87154315baef0b9db6787eacfd3027f0018bb93
eldarian-1 <ciborg.brain@gmail.com>
2020-10-23 11:54:52 +0500

    11:54 23.10.2020

--- OOP Labs/Lab8/Lab8/MainWindow.xaml.cs
+++ OOP Labs/Lab8/Lab8/MainWindow.xaml.cs
@@ -3,25 +3,82 @@ using System.Windows;
 using Microsoft.Win32;
 using System.Windows.Controls;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Windows.Input;
 
 namespace Lab8
 {
-    
-
     public partial class MainWindow : Window
     {
         public ClientList clients { get; set; }
+        public int updateIndex = -1;
 
         public MainWindow()
         {
             InitializeComponent();
             clients = new ClientList();
-            clientsList.ItemsSource = clients;
+            ListClient.ItemsSource = clients;
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            int index = ListClient.SelectedIndex;
+            if (index != -1)
+            {
+                switch (e.Key)
+                {
+                    case Key.W:
+                        MoveClient(index, index - 1);
+                        break;
+                    case Key.S:
+                        MoveClient(index, index + 1);
+                        break;
+                    case Key.E:
+                        UpdateClient(index);
+                        break;
+                    case Key.Delete:
+                        DeleteClient(index);
+                        break;
+                }
+            }
+        }
+
+        private void MoveClient(int oldIndex, int newIndex)
+        {
+            if (newIndex >= 0 && newIndex < clients.Count)
+            {
+                clients.Move(oldIndex, newIndex);
+                ListClient.SelectedIndex = newIndex;
+            }
+        }
+
+        private void UpdateClient(int index)
+        {
+            updateIndex = index;
+            ButtonClient.Click -= ButtonAddClient;
+            ButtonClient.Click += ButtonUpdateClient;
+            ButtonClient.Content = "Обновить";
+            Client client = clients[updateIndex];
+            ClientName.Text = client.Name;
+            ClientSumm.Text = client.Summ.ToString();
+            ClientType.Text = client.Type;
+            ClientPeriod.Text = client.Period;
+        }
+
+        private void DeleteClient(int index)
+        {
+            clients.RemoveAt(index);
+            ListClient.ItemsSource = clients;
+            if(updateIndex != -1)
+            {
+                updateIndex = -1;
+                ClearFields();
+            }
         }
 
         private void ButtonAddClient(object sender, RoutedEventArgs e)
         {
-            if (int.TryParse(ClientSumm.Text, out int summ))
+            if (int.TryParse(ClientSumm.Text, out int summ) && summ > 0)
+            {
                 clients.Add(new Client
                 {
                     Name = ClientName.Text,
@@ -29,6 +86,39 @@ namespace Lab8
                     Type = (ClientType.SelectedItem as ComboBoxItem).Content.ToString(),
                     Period = (ClientPeriod.SelectedItem as ComboBoxItem).Content.ToString()
                 });
+                ClearFields();
+            }
+            else
+                MessageBox.Show("Некорректное значение поля суммы!");
+        }
+
+        private void ButtonUpdateClient(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(ClientSumm.Text, out int summ) && summ > 0)
+            {
+                Client client = clients[updateIndex];
+                client.Name = ClientName.Text;
+                client.Summ = summ;
+                client.Type = (ClientType.SelectedItem as ComboBoxItem).Content.ToString();
+                client.Period = (ClientPeriod.SelectedItem as ComboBoxItem).Content.ToString();
+
+                ButtonClient.Click -= ButtonUpdateClient;
+                ButtonClient.Click += ButtonAddClient;
+                ClearFields();
+                updateIndex = -1;
+                ListClient.Items.Refresh();
+            }
+            else
+                MessageBox.Show("Некорректное значение поля суммы!");
+        }
+
+        private void ClearFields()
+        {
+            ButtonClient.Content = "Добавить";
+            ClientName.Text = "";
+            ClientSumm.Text = "";
+            ClientType.Text = "Микрозайм";
+            ClientPeriod.Text = "3 м";
         }
 
         private void ClickSave(object sender, RoutedEventArgs e)
@@ -57,24 +147,24 @@ namespace Lab8
                     BinaryFormatter buffer = new BinaryFormatter();
                     clients = (ClientList)buffer.Deserialize(fstream);
                     fstream.Close();
-                    clientsList.ItemsSource = clients;
+                    ListClient.ItemsSource = clients;
                 }
             }
         }
 
         private void ClickFindName(object sender, RoutedEventArgs e)
         {
-            clientsList.ItemsSource = clients.GetListByName(BoxFindName.Text);
+            ListClient.ItemsSource = clients.GetListByName(BoxFindName.Text);
         }
 
         private void ClickFindPeriod(object sender, RoutedEventArgs e)
         {
-            clientsList.ItemsSource = clients.GetListByPeriod((BoxFindPeriod.SelectedItem as ComboBoxItem).Content.ToString());
+            ListClient.ItemsSource = clients.GetListByPeriod((BoxFindPeriod.SelectedItem as ComboBoxItem).Content.ToString());
         }
 
         private void ClickReset(object sender, RoutedEventArgs e)
         {
-            clientsList.ItemsSource = clients;
+            ListClient.ItemsSource = clients;
         }
     }
 }

8825d3791aa0c4c7ec7d73d555c1c717c886dae7
eldarian-1 <ciborg.brain@gmail.com>
2020-12-06 23:27:09 +0500

    23:27 06.12.2020

---
+++ OOP Labs/Labs/Collection/SinglyLinkedList/Enumerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Collection.SinglyLinkedList
+{
+    internal class Enumerator<T> : IEnumerator<T>
+    {
+        private SinglyLinkedList<T> m_List;
+        private Node<T> m_Current;
+
+        object IEnumerator.Current => m_Current.Data;
+
+        public T Current => m_Current.Data;
+
+        public Enumerator(SinglyLinkedList<T> list)
+        {
+            m_List = list;
+            m_Current = list.Head;
+        }
+
+        public bool MoveNext()
+        {
+            bool flag = m_Current != null;
+            if (flag)
+                m_Current = m_Current.Next;
+            return flag;
+        }
+
+        public void Reset()
+        {
+            m_Current = m_List.Head;
+        }
+
+        public void Dispose() {}
+    }
+}
---
+++ OOP Labs/Labs/Collection/SinglyLinkedList/SinglyLinkedList.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Collection.SinglyLinkedList
+{
+    public class SinglyLinkedList<T> : IList<T>
+    {
+        internal Node<T> Head { get; private set; }
+
+        public int Count { get; private set; }
+
+        public bool IsReadOnly => true;
+
+        T IList<T>.this[int index]
+        {
+            get
+            {
+                if (index >= Count || index < 0)
+                    throw new Exception();
+                Node<T> temp = Head;
+                for (int i = 0; i < index; ++i)
+                    temp = temp.Next;
+                return temp.Data;
+            }
+            set
+            {
+                if (index >= Count || index < 0)
+                    throw new Exception();
+                Node<T> temp = Head;
+                for (int i = 0; i < index; ++i)
+                    temp = temp.Next;
+                temp.Data = value;
+            }
+        }
+
+        public SinglyLinkedList() {}
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index >= Count || index < 0)
+                    throw new Exception();
+                Node<T> temp = Head;
+                for (int i = 0; i < index; ++i)
+                    temp = temp.Next;
+                return temp.Data;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public int IndexOf(T item)
+        {
+            int index = 0;
+            Node<T> temp = Head;
+            while (!temp.Equals(item))
+            {
+                temp = temp.Next;
+                ++index;
+            }
+            return index;
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (index == 0 && Count == 0)
+                Add(item);
+            else
+            {
+                if (index > Count || index < 0)
+                    throw new Exception();
+                Node<T> temp = Head;
+                for (int i = 0; i < index; ++i)
+                    temp = temp.Next;
+                Node<T> newItem = new Node<T>() { Data = item, Next = temp.Next };
+                temp.Next = newItem;
+                ++Count;
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index >= Count || index < 0)
+                throw new Exception();
+            if(index == 0)
+                Head = Head.Next;
+            else
+            {
+                Node<T> temp = Head;
+                for (int i = 0; i < index - 1; ++i)
+                    temp = temp.Next;
+                temp.Next = temp.Next.Next;
+            }
+            ++Count;
+        }
+
+        public void Add(T item)
+        {
+            Node<T> newItem = new Node<T>() { Data = item };
+            if (Head == null)
+                Head = newItem;
+            else
+            {
+                Node<T> temp = Head;
+                for (int i = 0; i < Count; ++i)
+                    temp = temp.Next;
+                temp.Next = newItem;
+            }
+            ++Count;
+        }
+
+        public void Clear()
+        {
+            Head = null;
+        }
+
+        public bool Contains(T item)
+        {
+            bool flag = false;
+            foreach (T elem in this)
+                if (flag = item.Equals(elem))
+                    break;
+            return flag;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            int i = 0;
+            foreach (T item in this)
+                array[arrayIndex + i++] = item;
+        }
+
+        public bool Remove(T item)
+        {
+            bool flag = false;
+            Node<T> prev = null;
+            Node<T> curr = Head;
+            while(!flag && curr != null)
+            {
+                flag = item.Equals(curr);
+                if (flag)
+                    if (prev == null)
+                        Head = curr.Next;
+                    else
+                        prev.Next = curr.Next;
+                else
+                    prev = curr;
+                curr = curr.Next;
+            }
+            return flag;   
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new Enumerator<T>(this);
+        }
+    }
+}

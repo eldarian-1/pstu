4cd4e72f9b4c61e77f14ff9a192ef055d289502f
eldarian-1 <ciborg.brain@gmail.com>
2021-01-21 02:59:31 +0500

    2:59 21.01.2021

--- ООП/StudentDB/WpfUi/MainWindow.xaml.cs
+++ ООП/StudentDB/WpfUi/MainWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using Controller;
 using Model.Entities;
-using System.Collections;
 using System.Windows;
 
 namespace WpfUi
@@ -9,24 +8,50 @@ namespace WpfUi
     {
         private Facade _Facade;
 
-        public MainWindow()
+        private void Init()
         {
-            InitializeComponent();
             _Facade = new Facade();
-            SubjectFormItem.Parent = this;
-            UpdateData();
         }
 
-        public void UpdateData()
+        private void SetParent()
+        {
+            SubjectFormItem.Parent = this;
+            StudentFormItem.Parent = this;
+        }
+
+        private void SetData()
         {
             SubjectTableItem.SubjectList.ItemsSource = _Facade.Subjects.SelectAll(new Subject());
             StudentTableItem.StudentList.ItemsSource = _Facade.Students.SelectAll(new Student());
             MarkTableItem.MarkList.ItemsSource = _Facade.Marks.SelectAll(new MarkEntry());
         }
 
+        public MainWindow()
+        {
+            InitializeComponent();
+            Init();
+            SetParent();
+            SetData();
+        }
+
         public void AddSubject(string name)
         {
             _Facade.Subjects.Insert(new Subject { Name = name });
+        }
+
+        public void AddStudent(string firstName, string lastName)
+        {
+            _Facade.Students.Insert(new Student {
+                FirstName = firstName,
+                LastName = lastName });
+        }
+
+        public void AddMark(long studentId, long subjectId, byte value)
+        {
+            _Facade.Marks.Insert(new MarkEntry {
+                StudentId = studentId,
+                SubjectId = subjectId,
+                Value = value });
         }
     }
 }

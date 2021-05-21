35f763483810cf8b2e332ab06ca7cc7b35227ec0
eldarian <ciborg.brain@gmail.com>
2021-05-21 11:21:41 +0500

    11:21 21.05.2021

--- Сириус/18 Java Interface/sirius18/src/org/eldarian/task4/Solution.java
+++ Сириус/18 Java Interface/sirius18/src/org/eldarian/task4/Solution.java
@@ -4,18 +4,16 @@ import java.awt.*;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        System.out.println(new Fox().getName());
     }
+
     public interface Animal {
         String getName();
         Color getColor();
     }
-    public static class Fox implements Animal {
+
+    public static abstract class Fox implements Animal {
         public String getName() {
             return "Fox";
-        }
-        public Color getColor() {
-            return Color.ORANGE;
         }
     }
 }

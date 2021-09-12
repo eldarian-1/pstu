8825926d5dee2d6408533754ba5cfc22b14434b2
Эльдар Миннахметов <ciborg.brain@yandex.ru>
2021-09-12 13:53:01 +0500

    13:53 12.09.2021

--- Системное программирование/untitled/src/main/kotlin/Main.kt
+++ Системное программирование/untitled/src/main/kotlin/Main.kt
@@ -9,7 +9,7 @@ fun main() {
     //task3()
     //task4()
     //task5()
-    //task6()
+    task6()
 }
 
 fun task1() {
@@ -31,6 +31,8 @@ fun task1() {
         println("Деление на ноль!")
     } catch (e: IllegalArgumentException) {
         println("Вычисление корня из отрицательного числа!")
+    } catch (e: IndexOutOfBoundsException) {
+        println("Недостаточность входных данных!")
     }
 }
 
@@ -54,19 +56,28 @@ fun task2() {
         println(1 / (1 / r1 + 1 / r2 + 1 / r3))
     } catch (e: ArithmeticException) {
         println("Деление на ноль!")
+    } catch (e: IndexOutOfBoundsException) {
+        println("Недостаточность входных данных!")
     }
 }
 
 fun task3() {
     print("Введите числа x, y и z через пробел: ")
-    readLine()
-        ?.split(" ")
-        ?.map { it.toInt() }
-        .let {
-            val i = it?.filter { it % 3 == 0 }?.size
-            if(i == 2) println("Только 2 числа кратны 3")
-            else println("Условие, что только два числа кратны 3, не выполняется")
-        }
+    try {
+        readLine()
+            ?.split(" ")
+            ?.map { it.toInt() }
+            .let {
+                if(it?.size!! > 3) throw IllegalArgumentException()
+                val i = it?.filter { it % 3 == 0 }?.size
+                if (i == 2) println("Только 2 числа кратны 3")
+                else println("Условие, что только два числа кратны 3, не выполняется")
+            }
+    } catch (e: IndexOutOfBoundsException) {
+        println("Недостаточность входных данных!")
+    } catch (e: IllegalArgumentException) {
+        println("Недостаточность входных данных!")
+    }
 }
 
 fun task4() {
@@ -80,45 +91,62 @@ fun task4() {
             }
         }
     print("Введите номер месяца и год через пробел: ")
-    readLine()
-        ?.split(" ")
-        ?.map { it.toInt() }
-        .let {
-            val month = it!!.get(0)
-            val year = it.get(1)
-            println("${months[month - 1]} $year года: ${dayGetter.get(month - 1).apply(year)} дней")
-        }
+    try {
+        readLine()
+            ?.split(" ")
+            ?.map { it.toInt() }
+            .let {
+                val month = it!!.get(0)
+                val year = it.get(1)
+                println("${months[month - 1]} $year года: ${dayGetter.get(month - 1).apply(year)} дней")
+            }
+    } catch (e: IndexOutOfBoundsException) {
+        println("Недостаточность входных данных!")
+    }
 }
 
 fun task5() {
     var count = 0
     var sum = 0.0
     var predicate = true
-    while (predicate) {
-        predicate = readLine()?.toDouble().let { n ->
-            if (n == 0.0)
-                return@let false
-            else {
-                ++count
-                sum += n!!
-                return@let true
+    try {
+        while (predicate) {
+            print("Введите ${count + 1}-число (ноль прекратит ввод): ")
+            predicate = readLine()?.toDouble().let { n ->
+                if (n == 0.0) {
+                    if (count == 0) throw ArithmeticException()
+                    return@let false
+                } else {
+                    ++count
+                    sum += n!!
+                    return@let true
+                }
             }
         }
+        println("Среднее арифметическое введенных чисел: ${sum / count}")
+    } catch (e: Exception) {
+        println("Недостаточность входных данных!") // деление на ноль тем же обоснуется
     }
-    println("Среднее арифметическое введенных чисел: ${sum / count}")
 }
 
 fun task6() {
     print("Введите число N: ")
-    readLine()?.toInt().let { n ->
-        var cosSum = 0.0
-        var sinSum = 0.0
-        var multiply = 1.0
-        for(i in 1..n!!) {
-            cosSum += cos(i.toDouble())
-            sinSum += sin(i.toDouble())
-            multiply *= cosSum / sinSum
+    try {
+        readLine()?.toInt().let { n ->
+            var cosSum = 0.0
+            var sinSum = 0.0
+            var multiply = 1.0
+            if (n!! < 1) throw IllegalArgumentException()
+            for (i in 1..n) {
+                cosSum += cos(i.toDouble())
+                sinSum += sin(i.toDouble())
+                multiply *= cosSum / sinSum
+            }
+            println("Ответ: $multiply")
         }
-        println("Ответ: $multiply")
+    } catch (e: IllegalArgumentException) {
+        println("N не может быть меньше 1!")
+    } catch (e: Exception) {
+        println("Недостаточность входных данных!")
     }
 }

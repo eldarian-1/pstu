566eb6e9186252fb3ba65b5fda26139f56c50259
Эльдар Миннахметов <ciborg.brain@yandex.ru>
2021-09-20 10:03:12 +0500

    10:03 20.09.2021

--- Защита информации/untitled/RSATask.h
+++ Защита информации/untitled/RSATask.h
@@ -6,6 +6,33 @@
 
 #include "Task.h"
 
+class RSA {
+private:
+    static int gcd(int a, int b) {
+        if(a == b) return a;
+        if(a > b) return gcd(a - b, b);
+        else return gcd(a, b - a);
+    }
+
+
+public:
+    RSA(int p, int q) {
+        int n = p * q;
+        int e = -1;
+        int t = (p - 1) * (q - 1);
+        for(int i = n; i > 1; --i) {
+            int t0 = gcd(i, t);
+            if(t0 == 1) {
+                e = i;
+            }
+        }
+        if(e == -1) throw -1;
+
+    }
+
+
+};
+
 class RSATask: public Task {
 private:
     QLabel *label;

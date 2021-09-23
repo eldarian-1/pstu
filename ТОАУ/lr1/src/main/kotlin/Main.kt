f4527aa04ec0456e34af24f0a06565fd8f0a4636
Эльдар Миннахметов <ciborg.brain@yandex.ru>
2021-09-23 16:34:15 +0500

    16:34 23.09.2021

--- ТОАУ/lr1/src/main/kotlin/Main.kt
+++ ТОАУ/lr1/src/main/kotlin/Main.kt
@@ -1,16 +1,12 @@
 fun main() {
     try {
         print("Введите тариф (1 - первый, 2 - второй): ")
-        val r = readLine()
-            ?.toInt()
-            .takeIf { it == 1 || it == 2 }
+        val r = readLine()?.toInt().takeIf { it == 1 || it == 2 }
             ?: throw Exception("Некорректный тариф!")
         print("Введите числа K, x, %s через пробел: ".format(if(r == 1) "A, B" else "C, D"))
         val s = (readLine() ?: throw Exception("Некорректный ввод!"))
-            .split(" ")
-            .map { it.toDouble() }
-            .takeIf { it.size == 4 }
-            ?.let {
+            .split(" ").map { it.toDouble() }
+            .takeIf { it.size == 4 } ?.let {
                 val K = it[0]
                 val x = it[1]
                 val t1 = it[2]

8ff7405b4a51b0cb73a69e3de092f9e451138f70
Эльдар Миннахметов <ciborg.brain@yandex.ru>
2021-10-24 16:33:39 +0500

    16:33 24.10.2021

--- Защита информации/desktop/HuffmanTask.cpp
+++ Защита информации/desktop/HuffmanTask.cpp
@@ -6,6 +6,113 @@
 #include <QHBoxLayout>
 #include <QPushButton>
 #include <QFileDialog>
+#include <QMessageBox>
+
+HuffmanNode * HuffmanNode::get(char ch, int freq, HuffmanNode* left, HuffmanNode* right) {
+    HuffmanNode* node = new HuffmanNode();
+    node->ch = ch;
+    node->freq = freq;
+    node->left = left;
+    node->right = right;
+    return node;
+}
+
+bool HuffmanNode::operator()(HuffmanNode* l, HuffmanNode* r) {
+    return l->freq > r->freq;
+}
+
+void encode(HuffmanNode* root, string str, unordered_map<char, string> &huffmanCode) {
+    if (root == nullptr) {
+        return;
+    }
+
+    if (!root->left && !root->right) {
+        huffmanCode[root->ch] = str;
+    }
+
+    encode(root->left, str + "0", huffmanCode);
+    encode(root->right, str + "1", huffmanCode);
+}
+
+string decode(HuffmanNode* root, int &index, string str, string out) {
+    if (root == nullptr) {
+        return "";
+    }
+
+    if (!root->left && !root->right) {
+        return (out + root->ch);
+    }
+
+    index++;
+
+    if (str[index] == '0') {
+        return decode(root->left, index, str);
+    } else {
+        return decode(root->right, index, str);
+    }
+}
+
+HuffmanAlgorithm::HuffmanAlgorithm(string text) {
+    for (char ch: text) {
+        freq[ch]++;
+    }
+    for (auto pair: freq) {
+        pq.push(HuffmanNode::get(pair.first, pair.second, nullptr, nullptr));
+    }
+    while (pq.size() != 1) {
+        HuffmanNode *left = pq.top(); pq.pop();
+        HuffmanNode *right = pq.top();	pq.pop();
+
+        int sum = left->freq + right->freq;
+        pq.push(HuffmanNode::get('\0', sum, left, right));
+    }
+    root = pq.top();
+    ::encode(root, "", huffmanCode);
+
+    cout << "Huffman Codes are :\n" << '\n';
+    for (auto pair: huffmanCode) {
+        cout << pair.first << " " << pair.second << '\n';
+    }
+
+    cout << "\nOriginal string was :\n" << text << '\n';
+
+    string str = "";
+    for (char ch: text) {
+        str += huffmanCode[ch];
+    }
+
+    cout << "\nEncoded string is :\n" << str << '\n';
+
+    int index = -1;
+    cout << "\nDecoded string is: \n";
+    while (index < (int)str.size() - 2) {
+        ::decode(root, index, str);
+    }
+}
+
+string HuffmanAlgorithm::encode(string text) {
+    string str = "";
+    for (char ch: text) {
+        str += huffmanCode[ch];
+    }
+    return str;
+}
+
+string HuffmanAlgorithm::decode(string text) {
+    string str = "";
+    for (char ch: text) {
+        str += huffmanCode[ch];
+    }
+    return str;
+}
+
+istream& operator << (istream& in, HuffmanAlgorithm& algorithm) {
+    return in;
+}
+
+ostream& operator >> (ostream& out, HuffmanAlgorithm& algorithm) {
+    return out;
+}
 
 void HuffmanTask::initWidget(QWidget *wgt) {
     lytMain = new QVBoxLayout;
@@ -49,18 +156,42 @@ void HuffmanTask::initWidget(QWidget *wgt) {
     connect(btnUnpackArif, SIGNAL(released()), this, SLOT(unpackArif()));
 }
 
-void HuffmanTask::packHuff() {
+void HuffmanTask::doAnything(QLabel *lbl, QString (*fun)(QString)) {
+    QFile source(QFileDialog::getOpenFileName());
+    QFile target(QFileDialog::getSaveFileName());
+    lbl->setText(QString::asprintf(
+                    "From: %s\nTo: %s",
+                    source.fileName().toStdString().c_str(),
+                    target.fileName().toStdString().c_str()));
+    if(!source.open(QIODevice::ReadOnly) || !target.open(QIODevice::WriteOnly)) {
+        QMessageBox::warning(nullptr, "Предупреждение", "Исходный файл не выбран или не существует");
+        return;
+    }
+    target.write(fun(source.readAll()).toUtf8());
+    source.close();
+    target.close();
+}
 
+void HuffmanTask::packHuff() {
+    doAnything(lblPackHuff, [](QString s) -> QString {
+        return HuffmanAlgorithm(s.toStdString()).encode(s.toStdString()).c_str();
+    });
 }
 
 void HuffmanTask::unpackHuff() {
-
+    doAnything(lblUnpackHuff, [](QString s) -> QString {
+        return HuffmanAlgorithm(s.toStdString()).encode(s.toStdString()).c_str();
+    });
 }
 
 void HuffmanTask::packArif() {
-
+    doAnything(lblPackArif, [](QString s) -> QString {
+        return HuffmanAlgorithm(s.toStdString()).encode(s.toStdString()).c_str();
+    });
 }
 
 void HuffmanTask::unpackArif() {
-
+    doAnything(lblUnpackArif, [](QString s) -> QString {
+        return HuffmanAlgorithm(s.toStdString()).encode(s.toStdString()).c_str();
+    });
 }
--- Защита информации/desktop/HuffmanTask.h
+++ Защита информации/desktop/HuffmanTask.h
@@ -11,105 +11,41 @@
 #include <unordered_map>
 using namespace std;
 
-/*struct Node {
-    char ch;
-    int freq;
-    Node *left, *right;
-
-    static Node * get(char ch, int freq, Node* left, Node* right) {
-        Node* node = new Node();
-        node->ch = ch;
-        node->freq = freq;
-        node->left = left;
-        node->right = right;
-        return node;
-    }
-
-    bool operator()(Node* l, Node* r) {
-        return l->freq > r->freq;
-    }
-};
-
-void encode(Node* root, string str, unordered_map<char, string> &huffmanCode) {
-    if (root == nullptr) {
-        return;
-    }
-
-    if (!root->left && !root->right) {
-        huffmanCode[root->ch] = str;
-    }
-
-    encode(root->left, str + "0", huffmanCode);
-    encode(root->right, str + "1", huffmanCode);
-}
-
-string decode(Node* root, int &index, string str, string out = "") {
-    if (root == nullptr) {
-        return "";
-    }
-
-    if (!root->left && !root->right) {
-        return (out + root->ch);
-    }
-
-    index++;
-
-    if (str[index] == '0') {
-        return decode(root->left, index, str);
-    } else {
-        return decode(root->right, index, str);
-    }
-}
-
-void buildHuffmanTree(string text) {
-    unordered_map<char, int> freq;
-    for (char ch: text) {
-        freq[ch]++;
-    }
-
-    priority_queue<Node*, vector<Node*>, Node> pq;
-
-    for (auto pair: freq) {
-        pq.push(Node::get(pair.first, pair.second, nullptr, nullptr));
-    }
-
-    while (pq.size() != 1) {
-        Node *left = pq.top(); pq.pop();
-        Node *right = pq.top();	pq.pop();
-
-        int sum = left->freq + right->freq;
-        pq.push(Node::get('\0', sum, left, right));
-    }
-
-    Node* root = pq.top();
-
-    unordered_map<char, string> huffmanCode;
-    encode(root, "", huffmanCode);
-
-    cout << "Huffman Codes are :\n" << '\n';
-    for (auto pair: huffmanCode) {
-        cout << pair.first << " " << pair.second << '\n';
-    }
-
-    cout << "\nOriginal string was :\n" << text << '\n';
-
-    string str = "";
-    for (char ch: text) {
-        str += huffmanCode[ch];
-    }
-
-    cout << "\nEncoded string is :\n" << str << '\n';
-
-    int index = -1;
-    cout << "\nDecoded string is: \n";
-    while (index < (int)str.size() - 2) {
-        decode(root, index, str);
-    }
-}*/
-
 class QVBoxLayout;
 class QHBoxLayout;
 class QPushButton;
+
+struct HuffmanNode {
+    char ch;
+    int freq;
+    HuffmanNode *left, *right;
+
+    static HuffmanNode * get(char ch, int freq, HuffmanNode* left, HuffmanNode* right);
+    bool operator()(HuffmanNode* l, HuffmanNode* r);
+
+};
+
+void encode(HuffmanNode* root, string str, unordered_map<char, string> &huffmanCode);
+string decode(HuffmanNode* root, int &index, string str, string out = "");
+
+class HuffmanAlgorithm {
+private:
+    unordered_map<char, int> freq;
+    priority_queue<HuffmanNode*, vector<HuffmanNode*>, HuffmanNode> pq;
+    HuffmanNode* root;
+    unordered_map<char, string> huffmanCode;
+
+public:
+    HuffmanAlgorithm() {}
+    HuffmanAlgorithm(string text);
+
+    string encode(string text);
+    string decode(string text);
+
+    friend istream& operator << (istream& in, HuffmanAlgorithm& algorithm);
+    friend ostream& operator >> (ostream& out, HuffmanAlgorithm& algorithm);
+
+};
 
 class HuffmanTask: public QObject, public Task {
 Q_OBJECT
@@ -136,7 +72,10 @@ public:
     HuffmanTask(): Task("Алгоритм Хаффмана") {}
     void initWidget(QWidget *wgt) override;
 
-public slots:
+private:
+    void doAnything(QLabel *lbl, QString (*)(QString));
+
+private slots:
     void packHuff();
     void unpackHuff();
     void packArif();

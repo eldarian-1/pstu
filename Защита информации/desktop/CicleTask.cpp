f5856e8e6094c8fae30921c3a5fd32cc1171245d
Эльдар Миннахметов <ciborg.brain@yandex.ru>
2021-10-29 17:59:24 +0500

    17:59 29.10.2021

--- Защита информации/desktop/CicleTask.cpp
+++ Защита информации/desktop/CicleTask.cpp
@@ -12,10 +12,17 @@ void CicleTask::initWidget(QWidget *wgt) {
     lblName = new QLabel("Метод циклических кодов");
     lblSource = new QLabel("Отправляемый текст");
     leSource = new QLineEdit;
-    lblReceived = new QLabel("Полученный текст");
-    teTarget = new QTextEdit;
-    lblTarget = new QLabel;
+    teSource = new QTextEdit;
+    lblEncoded = new QLabel("Закодированный бинарный текст");
+    teEncoded = new QTextEdit;
+    lblDecoded = new QLabel("Декодированный текст");
+    teDecoded = new QTextEdit;
+    leDecoded = new QLineEdit;
     btnCorrect = new QPushButton("Исправить ошибки");
+
+    teSource->setDisabled(true);
+    teDecoded->setDisabled(true);
+    leDecoded->setDisabled(true);
 
     wgt->setLayout(lytMain);
     wgt->setFixedHeight(400);
@@ -23,26 +30,35 @@ void CicleTask::initWidget(QWidget *wgt) {
     lytMain->addWidget(lblName);
     lytMain->addWidget(lblSource);
     lytMain->addWidget(leSource);
-    lytMain->addWidget(lblReceived);
-    lytMain->addWidget(teTarget);
-    lytMain->addWidget(lblTarget);
+    lytMain->addWidget(teSource);
+    lytMain->addWidget(lblEncoded);
+    lytMain->addWidget(teEncoded);
+    lytMain->addWidget(lblDecoded);
+    lytMain->addWidget(leDecoded);
+    lytMain->addWidget(teDecoded);
     lytMain->addWidget(btnCorrect);
 
     connect(leSource, SIGNAL(textChanged(QString)), SLOT(sourceChanged(QString)));
-    connect(teTarget, SIGNAL(textChanged()), SLOT(targetChanged()));
+    connect(teEncoded, SIGNAL(textChanged()), SLOT(encodedChanged()));
     connect(btnCorrect, SIGNAL(released()), SLOT(correctReleased()));
 }
 
 void CicleTask::sourceChanged(const QString &text) {
-    teTarget->setText(bitsAsString(stringToBits(text)));
+    QBitArray bits = stringToBits(text);
+    QBitArray encoded = encode(bits);
+    teSource->setText(bitsAsString(bits));
+    teEncoded->setText(bitsAsString(encoded));
 }
 
-void CicleTask::targetChanged() {
-    lblTarget->setText(bitsToString(stringAsBits(teTarget->toPlainText())));
+void CicleTask::encodedChanged() {
+    QBitArray bits = stringAsBits(teEncoded->toPlainText());
+    QBitArray decoded = decode(bits);
+    teDecoded->setText(bitsAsString(decoded));
+    leDecoded->setText(bitsToString(decoded));
 }
 
 void CicleTask::correctReleased() {
-    teTarget->setText(bitsAsString(correct(stringToBits(leSource->text()))));
+    teEncoded->setText(bitsAsString(correct(stringAsBits(teSource->toPlainText()))));
 }
 
 QBitArray CicleTask::stringToBits(const QString &text) {
@@ -72,7 +88,7 @@ QString CicleTask::bitsToString(const QBitArray &bits) {
     return result;
 }
 
-QString CicleTask::bitsAsString(const QBitArray &&bits) {
+QString CicleTask::bitsAsString(const QBitArray &bits) {
     QString result;
     int size = bits.size();
     for(int i = 0; i < size; ++i) {
@@ -90,8 +106,84 @@ QBitArray CicleTask::stringAsBits(const QString &text) {
     return result;
 }
 
-QBitArray CicleTask::correct(const QBitArray &&bits) {
+QBitArray CicleTask::correct(const QBitArray &bits) {
     QBitArray result(bits);
     result[0] = 1;
+    return result;
+}
+
+QBitArray CicleTask::encode(const QBitArray &bits) {
+    QVector<QBitArray> devs = divide(bits, 4);
+    for(int i = 0, n = devs.size(); i < n; ++i) {
+        devs[i] = bit4to7(devs[i]);
+    }
+    return sum(devs, 7);
+}
+
+QBitArray CicleTask::decode(const QBitArray &bits) {
+    QVector<QBitArray> devs = divide(bits, 7);
+    for(int i = 0, n = devs.size(); i < n; ++i) {
+        devs[i] = bit7to4(devs[i]);
+    }
+    return sum(devs, 4);
+}
+
+QVector<QBitArray> CicleTask::divide(const QBitArray &bits, int denominator) {
+    int size = bits.size() / denominator;
+    QVector<QBitArray> result(size, QBitArray(denominator));
+    for(int i = 0; i < size; ++i) {
+        for(int j = 0; j < denominator; ++j) {
+            result[i][j] = bits[i * denominator + j];
+        }
+    }
+    return result;
+}
+
+QBitArray CicleTask::sum(const QVector<QBitArray> &bits, int denominator) {
+    int size = bits.size();
+    QBitArray result(size * denominator);
+    for(int i = 0; i < size; ++i) {
+        for(int j = 0; j < denominator; ++j) {
+            result[i * denominator + j] = bits[i][j];
+        }
+    }
+    return result;
+}
+
+QBitArray CicleTask::bit4to7(const QBitArray &bits) {
+    QBitArray result(7, 0);
+    auto sdvig = [](const QBitArray& in) -> QBitArray {
+        int size = in.size();
+        QBitArray result(size, 0);
+        for(int i = 1; i < size; ++i) {
+            result[i] = in[i - 1];
+        }
+        return result;
+    };
+    auto summod = [](const QBitArray& a, const QBitArray& b, const QBitArray& d, int i) -> int {
+        return (a[i] + b[i] + d[i]) % 2;
+    };
+    auto bitArray = [](const QBitArray& a, int size) -> QBitArray {
+        QBitArray result(size);
+        for(int i = 0, n = a.size(); i < n; ++i) {
+            result[i] = a[i];
+        }
+        return result;
+    };
+    auto a = bitArray(bits, 7);
+    auto b = sdvig(a);
+    auto c = sdvig(b);
+    auto d = sdvig(c);
+    for(int i = 0; i < 7; ++i) {
+        result[i] = summod(a, b, d, i);
+    }
+    return result;
+}
+
+QBitArray CicleTask::bit7to4(const QBitArray &bits) {
+    QBitArray result(4, 0);
+    for(int i = 0; i < 4; ++i) {
+        result[i] = bits[i];
+    }
     return result;
 }
--- Защита информации/desktop/CicleTask.h
+++ Защита информации/desktop/CicleTask.h
@@ -18,9 +18,12 @@ private:
     QLabel *lblName;
     QLabel *lblSource;
     QLineEdit *leSource;
-    QLabel *lblReceived;
-    QTextEdit *teTarget;
-    QLabel *lblTarget;
+    QTextEdit *teSource;
+    QLabel *lblEncoded;
+    QTextEdit *teEncoded;
+    QLabel *lblDecoded;
+    QTextEdit *teDecoded;
+    QLineEdit *leDecoded;
     QPushButton *btnCorrect;
 
 public:
@@ -32,14 +35,22 @@ public:
 
 private slots:
     void sourceChanged(const QString &text);
-    void targetChanged();
+    void encodedChanged();
     void correctReleased();
 
 private:
     static QBitArray stringToBits(const QString &text);
     static QString bitsToString(const QBitArray &bits);
-    static QString bitsAsString(const QBitArray &&bits);
+    static QString bitsAsString(const QBitArray &bits);
     static QBitArray stringAsBits(const QString &text);
-    static QBitArray correct(const QBitArray &&bits);
+    static QBitArray correct(const QBitArray &bits);
+
+    static QBitArray encode(const QBitArray &bits);
+    static QBitArray decode(const QBitArray &bits);
+
+    static QVector<QBitArray> divide(const QBitArray &bits, int denominator);
+    static QBitArray sum(const QVector<QBitArray> &bits, int denominator);
+    static QBitArray bit4to7(const QBitArray &bits);
+    static QBitArray bit7to4(const QBitArray &bits);
 
 };

aaefa52a9efa0ac52ced70a1e0ce8a2670788e98
Эльдар Миннахметов <ciborg.brain@yandex.ru>
2021-11-05 00:40:20 +0500

    00:40 05.11.2021

---
+++ Интернет вещей/holidays/server/main/views.py
@@ -0,0 +1,9 @@
+import datetime
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse(
+        "Работа студента группы РИС-19-1б<br>Эльдара Миннахметова<br>%s"
+        % datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+    )
